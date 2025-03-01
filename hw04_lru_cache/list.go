13e3d26b8761fdd7e2b531da070174fcdae0708d
dimryb <dimryb@bk.ru>
2025-03-01 20:23:11 +0700

    Реализовал двусвязный список

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -16,56 +16,101 @@ type ListItem struct {
 	Prev  *ListItem
 }
 
+type Empty struct{}
+
 type list struct {
-	// Place your code here.
+	data  map[*ListItem]Empty
+	front *ListItem
+	back  *ListItem
 }
+
+type position int
+
+const (
+	front position = iota
+	back
+)
 
 func NewList() List {
-	return new(list)
+	l := &list{
+		data: make(map[*ListItem]Empty),
+	}
+	return l
 }
 
-func (l *list) Len() int { return 0 }
+func (l *list) Len() int {
+	return len(l.data)
+}
 
 func (l *list) Front() *ListItem {
-	return &ListItem{
-		Value: nil,
-		Next:  nil,
-		Prev:  nil,
+	if l.Len() == 0 {
+		return nil
 	}
+	return l.front
 }
 
 func (l *list) Back() *ListItem {
-	return &ListItem{
-		Value: nil,
-		Next:  nil,
-		Prev:  nil,
+	if l.Len() == 0 {
+		return nil
 	}
+	return l.back
+}
+
+func (l *list) push(v interface{}, position position) *ListItem {
+	listItem := &ListItem{Value: v}
+
+	switch {
+	case l.Len() == 0:
+		l.front = listItem
+		l.back = listItem
+	case position == front:
+		listItem.Next = l.front
+		l.front.Prev = listItem
+		l.front = listItem
+	case position == back:
+		listItem.Prev = l.back
+		l.back.Next = listItem
+		l.back = listItem
+	}
+	l.data[listItem] = Empty{}
+
+	return listItem
 }
 
 func (l *list) PushFront(v interface{}) *ListItem {
-	return &ListItem{
-		Value: v,
-		Next:  nil,
-		Prev:  nil,
-	}
+	return l.push(v, front)
 }
 
 func (l *list) PushBack(v interface{}) *ListItem {
-	return &ListItem{
-		Value: v,
-		Next:  nil,
-		Prev:  nil,
-	}
+	return l.push(v, back)
 }
 
 func (l *list) Remove(i *ListItem) {
 	if i == nil {
 		return
 	}
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+	delete(l.data, i)
 }
 
 func (l *list) MoveToFront(i *ListItem) {
-	if i == nil {
+	if i == nil || l.front == i {
 		return
 	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+	if l.back == i {
+		l.back = i.Prev
+	}
+
+	i.Next = l.front
+	i.Prev = nil
+	l.front.Prev = i
+	l.front = i
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -3,8 +3,17 @@ package hw04lrucache
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func toList(l List) []int {
+	elems := make([]int, 0, l.Len())
+	for i := l.Front(); i != nil; i = i.Next {
+		elems = append(elems, i.Value.(int))
+	}
+	return elems
+}
 
 func TestList(t *testing.T) {
 	t.Run("empty list", func(t *testing.T) {
@@ -13,6 +22,82 @@ func TestList(t *testing.T) {
 		require.Equal(t, 0, l.Len())
 		require.Nil(t, l.Front())
 		require.Nil(t, l.Back())
+	})
+
+	t.Run("simple push front", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.PushFront(20) // [20, 10]
+		l.PushFront(30) // [30, 20, 10]
+
+		require.Equal(t, 3, l.Len())
+
+		elems := toList(l)
+		require.Equal(t, []int{30, 20, 10}, elems)
+	})
+
+	t.Run("simple push back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10) // [10]
+		l.PushBack(20) // [10, 20]
+		l.PushBack(30) // [10, 20, 30]
+
+		require.Equal(t, 3, l.Len())
+
+		elems := toList(l)
+		require.Equal(t, []int{10, 20, 30}, elems)
+	})
+
+	t.Run("remove", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+		l.PushBack(40)
+		l.PushBack(50)
+
+		require.Equal(t, 5, l.Len())
+
+		middle := l.Front().Next.Next
+		l.Remove(middle)
+
+		require.Equal(t, 4, l.Len())
+
+		elems := toList(l)
+		require.Equal(t, []int{10, 20, 40, 50}, elems)
+	})
+
+	t.Run("move to front", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+		l.PushBack(40)
+
+		item := l.Front().Next.Next
+		l.MoveToFront(item) // [30, 10, 20, 40]
+
+		elems := toList(l)
+		require.Equal(t, []int{30, 10, 20, 40}, elems)
+	})
+
+	t.Run("move to front end item", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10)
+		l.PushBack(20)
+		l.PushBack(30)
+		l.PushBack(40)
+
+		l.MoveToFront(l.Back()) // [40, 10, 20, 30]
+
+		elems := toList(l)
+		assert.Equal(t, []int{40, 10, 20, 30}, elems)
+		require.Equal(t, 30, l.Back().Value.(int))
 	})
 
 	t.Run("complex", func(t *testing.T) {
@@ -42,10 +127,7 @@ func TestList(t *testing.T) {
 		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
 		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
 
-		elems := make([]int, 0, l.Len())
-		for i := l.Front(); i != nil; i = i.Next {
-			elems = append(elems, i.Value.(int))
-		}
+		elems := toList(l)
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
 }
