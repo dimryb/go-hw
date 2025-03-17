9f57e2845daca561d14377c62c14473cd918e4bc
dimryb <dimryb@bk.ru>
2025-03-18 02:18:11 +0700

    Добавил промежуточный буферный канал для ускорения завершения

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -10,16 +10,31 @@ type Stage func(in In) (out Out)
 
 func runStage(in In, done In, stage Stage) Out {
 	out := make(Bi)
+	buf := make(Bi)
+
+	go func() {
+		defer close(buf)
+
+		stageOut := stage(in)
+		for v := range stageOut {
+			select {
+			case buf <- v:
+			case <-done:
+				for range stageOut {
+				}
+				return
+			}
+		}
+	}()
 
 	go func() {
 		defer close(out)
-		stageOut := stage(in)
 
 		for {
 			select {
 			case <-done:
 				return
-			case v, ok := <-stageOut:
+			case v, ok := <-buf:
 				if !ok {
 					return
 				}
