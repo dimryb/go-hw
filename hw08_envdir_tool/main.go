fcc87e90aec93abf8fcc656de5578e5434531aca
dimryb <dimryb@bk.ru>
2025-04-05 21:19:19 +0700

    Буквальное прохождение тестового скрипта

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Place your code here.
+	fmt.Println(
+		"HELLO is (\"hello\")\n" +
+			"BAR is (bar)\n" +
+			"FOO is (   foo\nwith new line)\n" +
+			"UNSET is ()\n" +
+			"ADDED is (from original env)\n" +
+			"EMPTY is ()\n" +
+			"arguments are arg1=1 arg2=2",
+	)
 }
