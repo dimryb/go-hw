6b725fb7074ba0bfc4b7abceb66a0633471da813
dimryb <dimryb@bk.ru>
2025-04-05 21:20:58 +0700

    Добавил чтение аргументов запуска

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go-envdir /path/to/env/dir command [args...]")
+		os.Exit(1)
+	}
+
+	envDir := os.Args[1]
+	command := os.Args[2]
+	args := os.Args[3:]
+
+	_ = envDir
+	_ = command
+	_ = args
+
 	fmt.Println(
 		"HELLO is (\"hello\")\n" +
 			"BAR is (bar)\n" +
