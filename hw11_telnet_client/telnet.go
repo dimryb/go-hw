1cae28b5993764bc0b2942856bc9f3b008dcaf9c
dimryb <dimryb@bk.ru>
2025-04-27 20:38:36 +0700

    Добавил основу приложения

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -12,10 +12,37 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
 }
 
 // Place your code here.
 // P.S. Author's solution takes no more than 50 lines.
+
+func (t *telnetClient) Connect() error {
+	return nil
+}
+
+func (t *telnetClient) Send() error {
+	return nil
+}
+
+func (t *telnetClient) Receive() error {
+	return nil
+}
+
+func (t *telnetClient) Close() error {
+	return t.in.Close()
+}
