b88bbc2fd6110c11c5be02cef0d67ceece9d5b6b
dimryb <dimryb@bk.ru>
2025-05-10 20:13:45 +0700

    Загрузка конфигурации и изменение структуры

--- hw12_13_14_15_16_calendar/internal/app/app.go
+++ hw12_13_14_15_16_calendar/internal/app/app.go
@@ -2,6 +2,16 @@ package app
 
 import (
 	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/dimryb/go-hw/hw12_13_14_15_calendar/internal/config"
+	"github.com/dimryb/go-hw/hw12_13_14_15_calendar/internal/logger"
+	internalhttp "github.com/dimryb/go-hw/hw12_13_14_15_calendar/internal/server/http"
+	memorystorage "github.com/dimryb/go-hw/hw12_13_14_15_calendar/internal/storage/memory"
 )
 
 type App struct {
@@ -15,10 +25,43 @@ type Logger interface { // TODO
 type Storage interface { // TODO
 }
 
-func New(logger Logger, storage Storage) *App {
-	return &App{
-		logger:  logger,
+func Run(configPath string) {
+	cfg, err := config.NewConfig(configPath)
+	if err != nil {
+		log.Fatalf("Config error: %s", err)
+	}
+
+	logg := logger.New(cfg.Log.Level)
+
+	storage := memorystorage.New()
+	calendar := &App{
+		logger:  logg,
 		storage: storage,
+	}
+
+	server := internalhttp.NewServer(logg, calendar)
+
+	ctx, cancel := signal.NotifyContext(context.Background(),
+		syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	defer cancel()
+
+	go func() {
+		<-ctx.Done()
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		if err := server.Stop(ctx); err != nil {
+			logg.Error("failed to stop http server: " + err.Error())
+		}
+	}()
+
+	logg.Info("calendar is running...")
+
+	if err := server.Start(ctx); err != nil {
+		logg.Error("failed to start http server: " + err.Error())
+		cancel()
+		os.Exit(1) //nolint:gocritic
 	}
 }
 
---
+++ hw12_13_14_15_16_calendar/internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"path"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type (
+	Config struct {
+		HTTP `yaml:"http"`
+		Log  `yaml:"log"`
+	}
+
+	HTTP struct {
+		Port string `yaml:"port" env:"HTTP_PORT"`
+	}
+
+	Log struct {
+		Level string `yaml:"level" env:"LOG_LEVEL"`
+	}
+)
+
+func NewConfig(configPath string) (*Config, error) {
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig(path.Join("./", configPath), cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	err = cleanenv.UpdateEnv(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error updating env: %w", err)
+	}
+
+	return cfg, nil
+}
+
+// TODO
