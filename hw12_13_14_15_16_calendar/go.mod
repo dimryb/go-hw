29eb5dfc34c3a33d00685591800fb276cac3b6b0
dimryb <dimryb@bk.ru>
2025-05-14 21:01:34 +0700

    Основа реализации хранилища через БД

--- hw12_13_14_15_16_calendar/go.mod
+++ hw12_13_14_15_16_calendar/go.mod
@@ -1,22 +1,30 @@
 module github.com/dimryb/go-hw/hw12_13_14_15_calendar
 
-go 1.22
+go 1.23.0
+
+toolchain go1.23.9
 
 require (
 	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/pressly/goose/v3 v3.24.3
 	github.com/sirupsen/logrus v1.9.3
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/stretchr/testify v1.10.0
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- hw12_13_14_15_16_calendar/internal/storage/sql/storage.go
+++ hw12_13_14_15_16_calendar/internal/storage/sql/storage.go
@@ -1,20 +1,66 @@
 package sqlstorage
 
-import "context"
+import (
+	"context"
+	"fmt"
 
-type Storage struct { // TODO
+	"github.com/jmoiron/sqlx"
+	"github.com/pressly/goose/v3"
+)
+
+type Config struct {
+	StorageType    string
+	DSN            string
+	MigrationsPath string
 }
 
-func New() *Storage {
-	return &Storage{}
+type Storage struct {
+	storageType    string
+	dsn            string
+	migrationsPath string
+	db             *sqlx.DB
+}
+
+func New(cfg Config) *Storage {
+	return &Storage{
+		storageType: cfg.StorageType,
+		dsn:         cfg.DSN,
+	}
 }
 
 func (s *Storage) Connect(ctx context.Context) error {
-	// TODO
+	db, err := sqlx.Connect(s.storageType, s.dsn)
+	if err != nil {
+		return fmt.Errorf("failed to connect to %s: %w", s.storageType, err)
+	}
+
+	s.db = db
+
+	if err := s.db.Ping(); err != nil {
+		return fmt.Errorf("failed to ping %s: %w", s.storageType, err)
+	}
+
 	return nil
 }
 
 func (s *Storage) Close(ctx context.Context) error {
-	// TODO
+	if s.db != nil {
+		if err := s.db.Close(); err != nil {
+			return fmt.Errorf("failed to close DB connection: %w", err)
+		}
+		s.db = nil
+	}
+	return nil
+}
+
+func (s *Storage) Migrate() error {
+	if err := goose.SetDialect(s.storageType); err != nil {
+		return fmt.Errorf("failed to set dialect: %w", err)
+	}
+
+	if err := goose.Up(nil, s.migrationsPath); err != nil {
+		return fmt.Errorf("failed to apply migrations: %w", err)
+	}
+
 	return nil
 }
