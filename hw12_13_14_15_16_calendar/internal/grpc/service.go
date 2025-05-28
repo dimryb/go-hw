6baab479da390f0b04e8de75caa90a8d5ce06cc8
dimryb <dimryb@bk.ru>
2025-05-29 00:01:03 +0700

    Добавил адаптеры события в прото

--- hw12_13_14_15_16_calendar/internal/grpc/service.go
+++ hw12_13_14_15_16_calendar/internal/grpc/service.go
@@ -1,6 +1,9 @@
 package grpc
 
 import (
+	"time"
+
+	"github.com/dimryb/go-hw/hw12_13_14_15_calendar/internal/storage/common"
 	"github.com/dimryb/go-hw/hw12_13_14_15_calendar/proto/calendar"
 )
 
@@ -10,4 +13,28 @@ type CalendarService struct {
 
 func NewCalendarService() *CalendarService {
 	return &CalendarService{}
+}
+
+func (s *CalendarService) protoToDomain(event *calendar.Event) storagecommon.Event {
+	return storagecommon.Event{
+		ID:           event.Id,
+		UserID:       event.UserId,
+		Title:        event.Title,
+		Description:  event.Description,
+		StartTime:    time.Unix(event.StartTime, 0),
+		EndTime:      time.Unix(event.EndTime, 0),
+		NotifyBefore: int(event.NotifyBefore),
+	}
+}
+
+func (s *CalendarService) domainToProto(event storagecommon.Event) *calendar.Event {
+	return &calendar.Event{
+		Id:           event.ID,
+		UserId:       event.UserID,
+		Title:        event.Title,
+		Description:  event.Description,
+		StartTime:    event.StartTime.Unix(),
+		EndTime:      event.EndTime.Unix(),
+		NotifyBefore: int64(event.NotifyBefore),
+	}
 }
